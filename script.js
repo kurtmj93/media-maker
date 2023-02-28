86ad0198c6dc54e7b5fbb4c5f1e01f89c93a8870
coolenedparrot <113797227+coolenedparrot@users.noreply.github.com>
2023-02-27 20:21:48 -0500

    About to design feed & profile page. GUI 3.0.

---
+++ script.js
@@ -0,0 +1,9 @@
+let welcomePage = document.querySelector("#welcomePage")
+
+document.querySelector("#signInButton").onclick = function(){
+    welcomePage.style.display = "none";
+};
+
+document.querySelector("#createAccountButton").onclick = function(){
+    welcomePage.style.display = "none";
+};
