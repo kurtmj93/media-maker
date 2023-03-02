6ee4cb3f3769cdad1cbf9605fce475e3d9941e2a
Cbw1981 <braxtonwilkey@gmail.com>
2023-03-02 18:27:23 -0500

    nodemailer

---
+++ views/script.js
@@ -0,0 +1,23 @@
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'outlook',
+  auth: {
+    user: 'mediamaker804@hotmail.com',
+    pass: 'TpI2H4U101!'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'clayton.wilkey@yahoo.com',
+  subject: 'New Account',
+  text: 'Welcome to Media Maker!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
